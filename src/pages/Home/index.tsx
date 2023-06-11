d41c1573011daa5f7527ce53e10b0977febae1d3
Caroline <carolineanjoss02@gmail.com>
2023-06-11 09:10:57 -0300

    creating a timer project, adding styling and routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+import { Play } from 'phosphor-react'
+
+export function Home() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="task">Vou trabalhar em</label>
+        <input id="task" />
+
+        <label htmlFor="minutesAmount">durante</label>
+        <input type="number" id="minutesAmount" />
+        <span>minutos.</span>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </div>
+  )
+}
